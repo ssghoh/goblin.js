4f04347e556adb4727d767d1093b408e931209dc
Hyunseok Oh <ssghoh@gmail.com>
2013-06-02 11:28:10 +0900

    * update systemInfo.js comment

--- js/goblin/systemInfo.js
+++ js/goblin/systemInfo.js
@@ -1,29 +1,45 @@
 /**
  * systemInfo.js
- * check game running system
+ * 게임 실행 환경을 검사 함
  * @author Hyunseok
  */
 var systemInfo = {
-	platform: null,	// web, mobile
-	device: null,	// ipad, iphone, android, pc
-	browser: null,	// chrome, ie, safari, firefox or opera
+	platform: null,		// web, mobile
+	device: null,		// ipad, iphone, android, pc
+	browser: null,		// chrome, ie, safari, firefox or opera
 	screenWidth: null,
 	screenHeight: null,
 	resolution: null,
 	orientation: null,
+/**
+ * 실행 기기의 화면 크기 정보 확인
+ */
 	checkDeviceScreen: function() {
-		this.screenWidth = 0;
-		this.screenHeight = 0;
+		this.screenWidth = window.innerWidth;
+		this.screenHeight = window.innerHeight;
 		console.log(this.screenWidth +" X " + this.screenHeight);
 	},
+/**
+ * 해상도 확인
+ */
 	checkResolution: function() {
 		this.resolution = this.screenWidth/this.screenHeight;
 		console.log(">> " + this.resolution); 
 	},
+/**
+ * 오리엔테이션 확인
+ */
 	checkOrientation:function() {
-		this.orientation = "portrait";
+		if(this.resolution < 1)
+			this.orientation = "portrait";
+		else
+			this.orientation = "landscape";
 		console.log(">> " + this.orientation);
 	},
+
+/**
+ * 시스템 정보 확인
+ */
 	checkSystem: function() {
 		console.log(navigator.appVersion);
 		console.log(navigator.userAgent);
